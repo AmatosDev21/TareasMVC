768cb6c9cc34543b77c0c0397dbd1ec571685aef
Alejandro Matos <isc.a.matos21@gmail.com>
2023-02-27 14:04:51 -0600

    Agregar archivos de proyecto.

---
+++ TareasMVC/Migrations/20230203203544_AdminRol.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF NOT EXISTS(Select Id from AspNetRoles 
+                                 where Id = '252c458f-b10a-4728-840f-77051811d4c4')
+                                 BEGIN
+	                             INSERT AspNetRoles (Id, [Name], [NormalizedName])
+	                             VALUES ('252c458f-b10a-4728-840f-77051811d4c4', 'admin', 'ADMIN')
+                                 END");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles Where Id = '252c458f-b10a-4728-840f-77051811d4c4'");
+        }
+    }
+}
---
+++ TareasMVC/Models/UsuarioListadoViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace TareasMVC.Models
+{
+    public class UsuarioListadoViewModel
+    {
+        public List<UsuarioViewModel> Usuarios { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
